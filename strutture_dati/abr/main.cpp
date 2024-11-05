39fa4da701b7812d317138439cb12d8ca7ae14b9
chxska <checcafrachifat@gmail.com>
2024-11-05 16:22:21 +0100

    aggiunta riservatezza

---
+++ strutture_dati/abr/main.cpp
@@ -0,0 +1,12 @@
+/*
+ * --------------------------------------------------------------------------------
+ * Codice sorgente di proprietà riservata.
+ * Tutti i diritti riservati © Checca.
+ *
+ * Questo codice è confidenziale e destinato esclusivamente all'uso autorizzato.
+ * È vietata la riproduzione, la distribuzione o la divulgazione di qualsiasi
+ * parte di questo codice senza il consenso esplicito di Checca.
+ *
+ * Autore: Checca
+ * --------------------------------------------------------------------------------
+ */
