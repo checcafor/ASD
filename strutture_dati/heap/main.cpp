afcce37427a37ffb81f550fc20a9341024f5f540
chxska <checcafrachifat@gmail.com>
2024-11-14 11:13:43 +0100

    max e min heap

--- strutture_dati/heap/main.cpp
+++ strutture_dati/heap/main.cpp
@@ -3,13 +3,15 @@
 #include <limits>
 
 enum Type {MIN, MAX};
+#define BIGMAX __INT_MAX__
+#define BIGMIN -__INT_MAX__
 
 using namespace std;
 
 class Heap {
     private:
         vector<int> heap;
-        int size; 
+        int size;
         Type type;
 
         int parent(int i) {return (i - 1) / 2;}
@@ -17,29 +19,27 @@ class Heap {
         int rightchild(int i) {return (i * 2) + 2;}
 
         void minheapify(int i) {
-            int left = leftchild(i);
-            int right = rightchild(i);
-
+            int left = leftchild(i), right = rightchild(i);
             int min = i;
-            if(left < size and heap[left] < heap[min])
+
+            if(left < size and heap[left] < heap[i])
                 min = left;
-            if(right < size and heap[right] < heap[min])
+            if(right < size and heap[right] < heap[i])
                 min = right;
 
-            if(i != min) {
+            if(min != i) {
                 swap(heap[i], heap[min]);
                 minheapify(min);
             }
         }
 
         void maxheapify(int i) {
-            int left = leftchild(i);
-            int right = rightchild(i);
+            int left = leftchild(i), right = rightchild(i);
+            int max = i; 
 
-            int max = i;
-            if(left < size and heap[left] > heap[max])
-                max = left;
-            if(right < size and heap[right] > heap[max])
+            if(left < size and heap[left] > heap [i])
+                max  = left;
+            if(right < size and heap[right] > heap[i])
                 max = right;
 
             if(max != i) {
@@ -52,17 +52,9 @@ class Heap {
             for(int i = (size / 2) - 1; i >= 0; i--)
                 (type == MIN) ? minheapify(i) : maxheapify(i);
         }
-    public:
-        Heap(vector<int> IN, Type t = MIN) {
-            heap = IN;
-            size = IN.size();
-            type = t;
-
-            buildheap();
-        }
 
         void decreasekey(int i, int key) {
-            if(i >= size or /*i < 0 or*/ key > heap[i])
+            if(i > size - 1 or key > heap[i])
                 return;
             
             heap[i] = key;
@@ -73,79 +65,86 @@ class Heap {
         }
 
         void increasekey(int i, int key) {
-            if(i >= size or /*i < 0 or*/ key < heap[i])
+            if(i > size - 1 or key < heap[i])
                 return;
             
             heap[i] = key;
-            while(i > 0 && heap[parent(i)] < heap[i]) {
+            while(i > 0 and heap[parent(i)] < heap[i]) {
                 swap(heap[i], heap[parent(i)]);
                 i = parent(i);
             }
         }
+    public:
+        Heap(vector<int> input, Type type = MIN) {
+            heap = input;
+            size = input.size();
+            this->type = type;
+            buildheap();
+        }
 
         void insert(int key) {
-            (type == MIN) ? heap.push_back(__INT_MAX__) : heap.push_back(std::numeric_limits<int>::min());
+            (type == MIN) ? (heap.push_back(BIGMAX)) : (heap.push_back(BIGMIN));
             size++;
             (type == MIN) ? decreasekey(size - 1, key) : increasekey(size - 1, key);
         }
 
-        int extractmin() {
-            if(type == MAX || size == 0) 
-                return std::numeric_limits<int>::max();
-            int min = heap[0];
-            heap[0] = heap[size - 1];
-            heap.pop_back();
-            size--;
-
-            minheapify(0);
-            return min;
-        }
-
-        int extractmax() {
-            if(type == MIN || size == 0) 
-                return std::numeric_limits<int>::min();
+        int extract_max() {
+            if(type == MIN)
+                return -1;
             int max = heap[0];
-            heap[0] = heap[size - 1];
-            heap.pop_back();
+            swap(heap[0], heap[size - 1]);
             size--;
 
             maxheapify(0);
             return max;
         }
 
-        void printheap() {
-            for(int i = 0; i < size; i++) {
-                cout << heap[i] << " ";
+        int extract_min() {
+            if(type == MAX)
+                return -1;
+            int min = heap[0];
+            swap(heap[0], heap[size - 1]);
+            size--;
+
+            minheapify(0);
+            return min;
+        }
+
+        void del(int num) {
+            int i;
+            for(i = 0; i < size; i++) 
+                if(heap[i] == num) {
+                    cout << "Elemento " << num << " trovato ed eliminato!" << endl;
+                    swap(heap[i], heap[size - 1]);
+                    break;
+                }
+            
+            heap.pop_back();
+            size--;
+            if(size > 0)
+                (type == MIN) ? minheapify(i) : maxheapify(i);
+        }
+
+        void print() {
+            (type == MIN) ? cout << "\nstampa minheap ..." << endl : cout << "\nstampa maxheap ..." << endl;
+            for(int el : heap) {
+                cout << " {" << el << "} \t";
             }
+
             cout << endl;
         }
 };
 
 int main() {
-    vector<int> data = {3, 1, 6, 5, 2, 4};
-    Heap minHeap(data, MIN);
-    
-    cout << "Min-Heap iniziale: ";
-    minHeap.printheap();
+    vector<int> input = {10, 3, 12, -1, 45, 8, 12, 78};
 
-    minHeap.insert(0);
-    cout << "Min-Heap dopo inserimento di 0: ";
-    minHeap.printheap();
+    Heap minheap(input);
+    minheap.print();
 
-    cout << "Estrazione del minimo: " << minHeap.extractmin() << endl;
-    minHeap.printheap();
+    minheap.del(3);
 
-    Heap maxHeap(data, MAX);
-    
-    cout << "Max-Heap iniziale: ";
-    maxHeap.printheap();
-
-    maxHeap.insert(7);
-    cout << "Max-Heap dopo inserimento di 7: ";
-    maxHeap.printheap();
-
-    cout << "Estrazione del massimo: " << maxHeap.extractmax() << endl;
-    maxHeap.printheap();
+    Heap maxheap(input, MAX);
+    maxheap.print();
 
     return 0;
 }
